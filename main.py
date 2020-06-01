c6bee8844a76d6266e0f77af0f929d5044464bbd
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-06-01 18:30:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from adafruit_circuitplayground import cp
 
 colours = [(25, 0, 0), (0, 25, 0), (0, 0, 25) ]
 
- 
+
 while True:
-    
+
     while cp.switch:
 
         luck = randint(1, 7)
 
         if luck == 1:
-            
+
             finalColor = choice(colours)
 
             cp.pixels[0] = finalColor
@@ -29,8 +29,10 @@ while True:
                 time.sleep(0.001)
 
                 timePassed+=0.001
-            
+
             print("Your reaction speed was " + str(timePassed) + " seconds.")
+
+            cp.play_tone(262, 1)
 
             time.sleep(2)
 
@@ -43,3 +45,13 @@ while True:
             cp.pixels[4] = choice(colours)
 
         time.sleep(1)
+        
+    if cp.switch == False:
+        
+        finalColor = [0,0,0]
+
+        cp.pixels[0] = finalColor
+        cp.pixels[1] = finalColor
+        cp.pixels[2] = finalColor
+        cp.pixels[3] = finalColor
+        cp.pixels[4] = finalColor
